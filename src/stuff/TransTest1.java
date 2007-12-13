770d4fe1a7bb064040051fca61365413e86efb54
Michael Kriese <ViceIce@users.noreply.github.com>
2007-12-13 11:41:13 +0000

    SoPra: - Transparent Tests

---
+++ src/stuff/TransTest1.java
@@ -0,0 +1,13 @@
+package stuff;
+
+public final class TransTest1 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	com.sun.jna.examples.ShapedWindowDemo.main(args);
+
+    }
+
+}
